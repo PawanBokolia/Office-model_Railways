c2603a3a79cf37e4f952618bd1bc41b8ed8788a0
PawanBokolia <pawanbokolia1@gmail.com>
2025-06-17 17:42:16 +0530

    office 17.6.25

---
+++ src/test/java/utilities/DataProviders.java
@@ -0,0 +1,31 @@
+package utilities;
+
+import java.io.IOException;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+	
+	
+	@DataProvider(name="jerrylogindata")
+	public String [][]  getdata() throws IOException
+	{
+		String path =".\\testData\\Jerry_Logintestdata.xlsx";
+		ExcelUtility xlutilw = new ExcelUtility (path);
+		
+		int trowNum = xlutilw.getRowCount("Sheet1");
+		int tcellNum = xlutilw.getCellCount("Sheet1", 1);
+		
+		String logindata[][] = new String [trowNum][tcellNum];
+		
+		for(int i =1; i<=trowNum; i++)
+		{
+			for(int j=0; j<tcellNum; j++)
+			{
+				logindata[i-1][j] = xlutilw.getCellData("Sheet1", i, j);
+			}
+		}
+		return logindata;
+	}
+}
+
